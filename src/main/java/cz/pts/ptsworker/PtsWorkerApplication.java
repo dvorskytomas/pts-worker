f3e806c6b7845cfd42daa3c7e99d328ffb90d281
Tomas Dvorsky <tomas.dvorsky@gmail.com>
2024-04-01 23:35:39 +0200

    Initial commit

---
+++ src/main/java/cz/pts/ptsworker/PtsWorkerApplication.java
@@ -0,0 +1,13 @@
+package cz.pts.ptsworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtsWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtsWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/pts/ptsworker/PtsWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.pts.ptsworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtsWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

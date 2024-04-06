6a5b85418fdce920956b98d6afd05cb9cb7c6a10
Tomas Dvorsky <tomas.dvorsky@gmail.com>
2024-04-06 23:27:28 +0200

    ptk-worker #1 - k6 exec method

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM eclipse-temurin:latest
 WORKDIR /opt/jmeter
 COPY jmeter .
 
+WORKDIR /opt/k6
+COPY k6 .
+
 WORKDIR /
 COPY /target/pts-worker-0.0.1-SNAPSHOT.jar app.jar
 
--- src/main/java/cz/pts/ptsworker/ExecController.java
+++ src/main/java/cz/pts/ptsworker/ExecController.java
@@ -41,6 +41,34 @@ public class ExecController {
         t.start();
     }
 
+    @PostMapping("/k6")
+    public void executeTestk6(){
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.command(
+                "/usr/bin/sh",
+                "-c",
+                "cd /opt/k6 && ./k6 run --vus 1 --duration 30s firstTest.js --out csv=results.csv"
+        );
+        pb.redirectErrorStream(true);
+
+        Thread t = new Thread(() -> {
+            Process p;
+            try {
+                System.out.println("Starting process builder in different thread.");
+                p = pb.start();
+
+                printStream(p.getInputStream());
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        t.start();
+    }
+
     private void printStream(InputStream inputStream) throws IOException {
         try(BufferedReader bufferedReader = new BufferedReader(
                 new InputStreamReader(inputStream)
